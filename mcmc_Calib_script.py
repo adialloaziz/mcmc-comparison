f2dc59fc1da1f3f1b9a64f4c49ecaad1af1afa9d
Aziz <163223321+adialloaziz@users.noreply.github.com>
2024-06-17 11:36:52 +1000

    Update mcmc_Calib_script.py

--- mcmc_Calib_script.py
+++ mcmc_Calib_script.py
@@ -6,9 +6,9 @@
 import multiprocessing as mp
 import platform
 
-if platform.system() != "Windows":
+# if platform.system() != "Windows":
 
-    mp.set_start_method('spawn')
+#     mp.set_start_method('spawn')
 
     # mp.set_start_method('forkserver')
 
@@ -41,70 +41,76 @@ import arviz as az
 
 from pathlib import Path
 
+if __name__ == "__main__":
+    if platform.system() != "Windows":
+    
+        mp.set_start_method('spawn')
 
-BASE_PATH = Path(__file__).parent.resolve()
-DATA_PATH = BASE_PATH / "data"
-
-targets_yml = Path(DATA_PATH/"target_yml.yml")
-
-
-#Names of parameters and their ranges
-params = {
-    "contact_rate": (0.0,1.0),
-    "recovery_rate": (0.0,1.0)
-
-}
-targets = get_targets(targets_yml) 
-priors = get_all_priors(params)
-
-#_______Model definition : A bayesian compartmental model with estival/SUMMER_____
-model_1 = model1()
-
-
-#________________Default parameters____________
-parameters = {
-    "contact_rate": 0.2,
-    "recovery_rate": 0.1,
-    #"active_cases_dispersion": 0.5,
-}
-bcm_model_1 = BayesianCompartmentalModel(model_1, parameters, priors, targets)
-# print(bcm_model_1)
-
-#_____________SAMPLING PROCESS________________________________________________________
-
-D = 2 # Dimension of the parameter's space
-sampler = pm.Metropolis #[infer.NUTS] # [pm.DEMetropolisZ]*2 + [pm.DEMetropolis]*2 + [pm.Metropolis]*4
-draws = 2000 #[4000]*6+ [8000]*2
-tune = 1000 #[100] + [1000] + [100] + [1000] + [100] + [1000] + [100] + [1000]
-chains = 2*D
-results = []
-
-
-
-#     #print(sampler)
-idata, Time = cal.Sampling_calib(
-    bcm_model = bcm_model_1,
-    mcmc_algo = sampler,
-    initial_params = parameters,
-    draws = draws,
-    tune = tune,
-    cores = 4,
-    chains = chains,
-        )
-
-results.append(cal.Compute_metrics(
+    print ("I'm here")
+    
+    BASE_PATH = Path(__file__).parent.resolve()
+    DATA_PATH = BASE_PATH / "data"
+    
+    targets_yml = Path(DATA_PATH/"target_yml.yml")
+    
+    
+    #Names of parameters and their ranges
+    params = {
+        "contact_rate": (0.0,1.0),
+        "recovery_rate": (0.0,1.0)
+    
+    }
+    targets = get_targets(targets_yml) 
+    priors = get_all_priors(params)
+    
+    #_______Model definition : A bayesian compartmental model with estival/SUMMER_____
+    model_1 = model1()
+    
+    
+    #________________Default parameters____________
+    parameters = {
+        "contact_rate": 0.2,
+        "recovery_rate": 0.1,
+        #"active_cases_dispersion": 0.5,
+    }
+    bcm_model_1 = BayesianCompartmentalModel(model_1, parameters, priors, targets)
+    # print(bcm_model_1)
+    
+    #_____________SAMPLING PROCESS________________________________________________________
+    
+    D = 2 # Dimension of the parameter's space
+    sampler = pm.Metropolis #[infer.NUTS] # [pm.DEMetropolisZ]*2 + [pm.DEMetropolis]*2 + [pm.Metropolis]*4
+    draws = 2000 #[4000]*6+ [8000]*2
+    tune = 1000 #[100] + [1000] + [100] + [1000] + [100] + [1000] + [100] + [1000]
+    chains = 2*D
+    results = []
+    
+    
+    
+    #     #print(sampler)
+    idata, Time = cal.Sampling_calib(
+        bcm_model = bcm_model_1,
         mcmc_algo = sampler,
-        idata = idata,
-        Time = Time,
-        draws = draws, 
-        chains = chains,
+        initial_params = parameters,
+        draws = draws,
         tune = tune,
+        cores = 4,
+        chains = chains,
             )
-        )
-
-
-results_df = pd.concat(results)
-# results_df["Run"] = results_df.Sampler + "\nDraws=" + results_df.Chains.astype(str) + "\nTune=" + results_df.Tune.astype(str)
-
-# results_df = results_df.reset_index(drop=True)
-# results_df.style.set_caption("MCMC COMPARISON")
+    
+    results.append(cal.Compute_metrics(
+            mcmc_algo = sampler,
+            idata = idata,
+            Time = Time,
+            draws = draws, 
+            chains = chains,
+            tune = tune,
+                )
+            )
+    
+    
+    results_df = pd.concat(results)
+    # results_df["Run"] = results_df.Sampler + "\nDraws=" + results_df.Chains.astype(str) + "\nTune=" + results_df.Tune.astype(str)
+    
+    # results_df = results_df.reset_index(drop=True)
+    # results_df.style.set_caption("MCMC COMPARISON")
